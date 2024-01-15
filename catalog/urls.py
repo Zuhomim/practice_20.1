9bc2f4fa220183360a60e5a8c5426ed24c98847a
Dmitri Sementsov <pluribus.zuhi@gmail.com>
2024-01-15 22:36:23 +0300

    add forms, Version_model

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
 from catalog.apps import CatalogConfig
-from catalog.views import CategoryListView, contacts, ProductListView, ProductDetailView
+from catalog.views import CategoryListView, contacts, ProductListView, ProductDetailView, ProductCreateView, \
+    ProductUpdateView
 
 app_name = CatalogConfig.name
 
@@ -9,5 +10,7 @@ urlpatterns = [
     path('', CategoryListView.as_view(), name='catalog'),
     path('contacts/', contacts, name='contacts'),
     path('<int:pk>/products/', ProductListView.as_view(), name='list_product'),
-    path('product/<int:pk>', ProductDetailView.as_view(), name='view_product')
+    path('product/<int:pk>', ProductDetailView.as_view(), name='view_product'),
+    path('<int:pk>/product/create/', ProductCreateView.as_view(), name='product_create'),
+    path('product/<int:pk>/update/', ProductUpdateView.as_view(), name='product_update'),
 ]
