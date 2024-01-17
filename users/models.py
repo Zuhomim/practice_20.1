f6b08c3bde23cec158b128d441af4674f41647ad
Dmitri Sementsov <pluribus.zuhi@gmail.com>
2024-01-18 02:32:52 +0300

    fixed confirm_reg

--- users/models.py
+++ users/models.py
@@ -1,3 +1,4 @@
+import uuid
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
@@ -11,6 +12,7 @@ class User(AbstractUser):
     avatar = models.ImageField(upload_to='users/', verbose_name='аватар', **NULLABLE)
     phone = models.CharField(max_length=30, verbose_name='телефон', **NULLABLE)
     country = models.CharField(max_length=30, verbose_name='страна', **NULLABLE)
+    uuid = models.CharField(primary_key=True, default=uuid.uuid4, editable=False, max_length=36)
 
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = []
